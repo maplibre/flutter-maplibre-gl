fe8478cd4c0353d01d8462dbf39a632e46fd219c
m0nac0 <58807793+m0nac0@users.noreply.github.com>
2021-03-08 20:35:50 +0100

    Update MapboxMapsPlugin due to Refactoring

--- ios/Classes/MapboxMapsPlugin.m
+++ ios/Classes/MapboxMapsPlugin.m
@@ -1,5 +1,5 @@
 #import "MapboxMapsPlugin.h"
-#import <mapbox_gl/mapbox_gl-Swift.h>
+#import <maplibre_gl/maplibre_gl-Swift.h>
 
 @implementation MapboxMapsPlugin 
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
