eac9423772f06f51c77198698f325f8aeb225d57
m0nac0 <58807793+m0nac0@users.noreply.github.com>
2021-10-20 19:16:27 +0200

    Fix linter hint (another unnecessary import)

--- maplibre_gl_platform_interface/lib/maplibre_gl_platform_interface.dart
+++ maplibre_gl_platform_interface/lib/maplibre_gl_platform_interface.dart
@@ -8,7 +8,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:meta/meta.dart' show visibleForTesting;
 
 part 'src/callbacks.dart';
 part 'src/camera.dart';
