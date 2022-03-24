45aea7dabd77c02d73fb9fcda624c7c2fe2a3ec6
Felix Horvat <felix.horvat@ocell.aero>
2022-03-24 14:02:54 +0100

    fixed LocationTracking issue delayed permissions (#958)
    * fixed LocationTracking issue delayed permissions
    
    * code review changes

--- android/src/main/java/com/mapbox/mapboxgl/MapboxMapController.java
+++ android/src/main/java/com/mapbox/mapboxgl/MapboxMapController.java
@@ -217,7 +217,6 @@ final class MapboxMapController
     mapView.addOnDidBecomeIdleListener(this);
 
     setStyleString(styleStringInitial);
-    // updateMyLocationEnabled();
   }
 
   @Override
@@ -1414,6 +1413,10 @@ final class MapboxMapController
 
   @Override
   public void setMyLocationTrackingMode(int myLocationTrackingMode) {
+    if (mapboxMap != null) {
+      // ensure that location is trackable
+      updateMyLocationEnabled();
+    }
     if (this.myLocationTrackingMode == myLocationTrackingMode) {
       return;
     }
