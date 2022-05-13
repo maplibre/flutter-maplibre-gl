bfda93c701f1b47cb120c77a8cae87df8f194d2c
m0nac0 <58807793+m0nac0@users.noreply.github.com>
2022-05-13 17:49:10 +0200

    fix iOS build error

--- ios/Classes/LayerPropertyConverter.swift
+++ ios/Classes/LayerPropertyConverter.swift
@@ -2,7 +2,7 @@
 // ./scripts/lib/generate.dart
 
 import Mapbox
-import MapboxAnnotationExtension
+import MapLibreAnnotationExtension
 
 class LayerPropertyConverter {
     class func addSymbolProperties(symbolLayer: MGLSymbolStyleLayer, properties: [String: String]) {
