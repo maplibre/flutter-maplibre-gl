98fccd7edaabbbafa4ddc2f6867c9cdcdfbdadd3
m0nac0 <58807793+m0nac0@users.noreply.github.com>
2022-05-22 14:59:53 +0200

    fix lint

--- example/lib/sources.dart
+++ example/lib/sources.dart
@@ -2,7 +2,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:maplibre_gl/mapbox_gl.dart';
 
-import 'main.dart';
 import 'page.dart';
 import 'package:maplibre_gl_platform_interface/maplibre_gl_platform_interface.dart';
 
