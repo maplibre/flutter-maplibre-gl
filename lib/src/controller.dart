9510e1c1a9ac35b75bcb5701dc5672a5d2351910
m0nac0 <58807793+m0nac0@users.noreply.github.com>
2022-06-27 21:38:00 +0200

    cherry-pick: Adding function in annotation manager to remove multiple annotations … (#120)
    Co-authored-by: eav-pgbalgi <99437759+eav-pgbalgi@users.noreply.github.com>

--- lib/src/controller.dart
+++ lib/src/controller.dart
@@ -601,9 +601,7 @@ class MaplibreMapController extends ChangeNotifier {
   ///
   /// The returned [Future] completes once listeners have been notified.
   Future<void> removeSymbols(Iterable<Symbol> symbols) async {
-    for (var symbol in symbols) {
-      await symbolManager!.remove(symbol);
-    }
+    await symbolManager!.removeAll(symbols);
     notifyListeners();
   }
 
@@ -693,10 +691,7 @@ class MaplibreMapController extends ChangeNotifier {
   ///
   /// The returned [Future] completes once listeners have been notified.
   Future<void> removeLines(Iterable<Line> lines) async {
-    for (var line in lines) {
-      await lineManager!.remove(line);
-    }
-
+    await lineManager!.removeAll(lines);
     notifyListeners();
   }
 
@@ -790,9 +785,7 @@ class MaplibreMapController extends ChangeNotifier {
   ///
   /// The returned [Future] completes once listeners have been notified.
   Future<void> removeCircles(Iterable<Circle> circles) async {
-    for (var circle in circles) {
-      await circleManager!.remove(circle);
-    }
+    await circleManager!.removeAll(circles);
     notifyListeners();
   }
 
@@ -891,10 +884,7 @@ class MaplibreMapController extends ChangeNotifier {
   ///
   /// The returned [Future] completes once listeners have been notified.
   Future<void> removeFills(Iterable<Fill> fills) async {
-    for (var fill in fills) {
-      await fillManager!.remove(fill);
-    }
-
+    await fillManager!.removeAll(fills);
     notifyListeners();
   }
 
