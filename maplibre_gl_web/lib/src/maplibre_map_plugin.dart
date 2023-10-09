e9b3d14829a6a71b823b1d58077bb2dd0f9f97df
m0nac0 <58807793+m0nac0@users.noreply.github.com>
2023-10-09 20:07:51 +0200

    update comment to point to MapLibreGlPlatform

--- maplibre_gl_web/lib/src/maplibre_map_plugin.dart
+++ maplibre_gl_web/lib/src/maplibre_map_plugin.dart
@@ -1,7 +1,7 @@
 part of maplibre_gl_web;
 
 class MaplibreMapPlugin {
-  /// Registers this class as the default instance of [MapboxGlPlatform].
+  /// Registers this class as the default instance of [MapLibreGlPlatform].
   static void registerWith(Registrar registrar) {
     MapLibreGlPlatform.createInstance = () => MaplibreMapController();
   }
