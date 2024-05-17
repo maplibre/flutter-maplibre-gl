c80e9ae089fd0860113fd4f5e7e77efcffe30329
Peter Leibiger <kuhnroyal@gmail.com>
2024-05-18 01:04:41 +0200

    Replace TextScaler usage with lager textStyle

--- example/lib/sources.dart
+++ example/lib/sources.dart
@@ -348,8 +348,8 @@ class FullMapState extends State<FullMap> {
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: Text(
-                    "Current source ${styleInfo.name}",
-                    textScaler: const TextScaler.linear(1.4),
+                    "Current source: ${styleInfo.name}",
+                    style: Theme.of(context).textTheme.titleLarge,
                   ),
                 ),
               ),
