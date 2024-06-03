b9fc96471760fe42221d87cd84780a619b2ecb25
Joscha <34318751+josxha@users.noreply.github.com>
2024-06-03 12:52:09 +0200

    resolve https://github.com/maplibre/flutter-maplibre-gl/pull/435#discussion_r1624193232

--- lib/maplibre_gl.dart
+++ lib/maplibre_gl.dart
@@ -43,7 +43,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:maplibre_gl/maplibre_gl.dart';
 
 import 'package:maplibre_gl_platform_interface/maplibre_gl_platform_interface.dart';
 
