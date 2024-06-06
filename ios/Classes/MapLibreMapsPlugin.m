d5cd7007260675e0e31653a3f42068aa4e09a383
Peter Leibiger <kuhnroyal@gmail.com>
2024-06-06 18:48:33 +0200

    Migrate Objective-C plugin to Swift

--- ios/Classes/MapLibreMapsPlugin.m
+++
@@ -1,8 +0,0 @@
-#import "MapLibreMapsPlugin.h"
-#import <maplibre_gl/maplibre_gl-Swift.h>
-
-@implementation MapLibreMapsPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftMapLibreGlFlutterPlugin registerWithRegistrar:registrar];
-}
-@end
