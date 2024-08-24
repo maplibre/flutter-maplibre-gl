8d55eb439c28a5dcf5a0209afab63a1bbeaf7ea5
Christos Iakovidis <ciakov@gmail.com>
2024-08-24 21:13:12 +0300

    Globals.setConnected handle all possible values

--- maplibre_gl/android/src/main/java/org/maplibre/maplibregl/GlobalMethodHandler.java
+++ maplibre_gl/android/src/main/java/org/maplibre/maplibregl/GlobalMethodHandler.java
@@ -79,8 +79,8 @@ class GlobalMethodHandler implements MethodChannel.MethodCallHandler {
         result.success(null);
         break;
       case "setOffline":
-        boolean offline = methodCall.argument("offline");
-        ConnectivityReceiver.instance(context).setConnected(offline ? false : null);
+        Boolean offline = methodCall.argument("offline");
+        ConnectivityReceiver.instance(context).setConnected(offline!=null ? !offline : null);
         result.success(null);
         break;
       case "mergeOfflineRegions":
