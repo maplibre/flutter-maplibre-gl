88975c0d04b4a012043f2945e0acbfa1ed655615
Albert Moravec <albert.moravec@gmail.com>
2024-12-13 14:23:28 +0100

    fix: Return error when location unavailable on request

--- maplibre_gl/android/src/main/java/org/maplibre/maplibregl/MapLibreMapController.java
+++ maplibre_gl/android/src/main/java/org/maplibre/maplibregl/MapLibreMapController.java
@@ -1273,6 +1273,11 @@ final class MapLibreMapController
                     result.error("", "", null); // ???
                   }
                 });
+          } else {
+            result.error(
+                "LOCATION DISABLED",
+                "Location is disabled or location component is unavailable",
+                null);
           }
           break;
         }
