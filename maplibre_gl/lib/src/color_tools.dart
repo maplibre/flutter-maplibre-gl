87047a08464ddc584a617e52cbd259dedc9558fb
Louis <louisraverdy@gmail.com>
2024-12-17 13:27:51 +0100

    Update color_tools.dart

--- maplibre_gl/lib/src/color_tools.dart
+++ maplibre_gl/lib/src/color_tools.dart
@@ -2,9 +2,9 @@ part of '../maplibre_gl.dart';
 
 extension MapLibreColorConversion on Color {
   String toHexStringRGB() {
-    final r = this.r.toRadixString(16).padLeft(2, '0');
-    final g = this.g.toRadixString(16).padLeft(2, '0');
-    final b = this.b.toRadixString(16).padLeft(2, '0');
+    final r = red.toRadixString(16).padLeft(2, '0');
+    final g = green.toRadixString(16).padLeft(2, '0');
+    final b = blue.toRadixString(16).padLeft(2, '0');
     return '#$r$g$b';
   }
 }
