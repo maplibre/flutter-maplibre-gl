90338e75f3f9b1d763bd119b40f266cd08c4f3e5
MichaelNeufeld <michael.neufeld@ocell.io>
2025-04-03 15:33:32 +0200

    fixed access

--- maplibre_gl_web/lib/src/maplibre_web_gl_platform.dart
+++ maplibre_gl_web/lib/src/maplibre_web_gl_platform.dart
@@ -70,8 +70,8 @@ class MapLibreMapController extends MapLibrePlatform
           bearing: camera['bearing'],
           pitch: camera['tilt'],
           attributionControl: false, //avoid duplicate control
-          minZoom: _creationParams["minMaxZoomPreference"][0],
-          maxZoom: _creationParams["minMaxZoomPreference"][1],
+          minZoom: _creationParams["options"]["minMaxZoomPreference"][0],
+          maxZoom: _creationParams["options"]["minMaxZoomPreference"][1],
         ),
       );
       _map.on('load', _onStyleLoaded);
