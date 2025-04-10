70b0792524de80aee2eff37c01eff6889a29b7e8
Felix Horvat <felix.horvat@ocell.aero>
2025-04-11 00:52:45 +0200

    fixed issues with style loading  * web did a diff update - native do full updates  * web did not wait fully for the style load

--- maplibre_gl_web/lib/src/maplibre_web_gl_platform.dart
+++ maplibre_gl_web/lib/src/maplibre_web_gl_platform.dart
@@ -73,7 +73,7 @@ class MapLibreMapController extends MapLibrePlatform
           attributionControl: false, //avoid duplicate control
         ),
       );
-      _map.on('load', _onStyleLoaded);
+      _map.on('style.load', _onStyleLoaded);
       _map.on('click', _onMapClick);
       // long click not available in web, so it is mapped to double click
       _map.on('dblclick', _onMapLongClick);
@@ -417,6 +417,13 @@ class MapLibreMapController extends MapLibrePlatform
   }
 
   void _onStyleLoaded(_) {
+    final loaded = _map.isStyleLoaded();
+    if (!loaded) {
+      Future.delayed(const Duration(milliseconds: 100), () {
+        _onStyleLoaded(_);
+      });
+      return;
+    }
     _mapReady = true;
     _onMapResize();
     onMapStyleLoadedPlatform(null);
@@ -702,11 +709,7 @@ class MapLibreMapController extends MapLibrePlatform
     }
     _interactiveFeatureLayerIds.clear();
 
-    _map.setStyle(styleString);
-    // catch style loaded for later style changes
-    if (_mapReady) {
-      _map.once("styledata", _onStyleLoaded);
-    }
+    _map.setStyle(styleString, {'diff': false});
   }
 
   @override
