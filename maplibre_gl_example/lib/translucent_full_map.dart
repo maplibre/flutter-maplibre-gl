17c54f628f6a0b752f19d297635396857edea5c7
Gabriel Palmisano <gabrielpalmisano63@gmail.com>
2025-07-13 16:51:14 +0200

    Update pubspec.yaml to use local maplibre_gl path and add translucent_style.json asset. Include TranslucentFullMapPage in example pages.

---
+++ maplibre_gl_example/lib/translucent_full_map.dart
@@ -0,0 +1,73 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:maplibre_gl/maplibre_gl.dart';
+
+import 'page.dart';
+
+const _nullIsland = CameraPosition(target: LatLng(0, 0), zoom: 4.0);
+
+class TranslucentFullMapPage extends ExamplePage {
+  const TranslucentFullMapPage({super.key})
+      : super(const Icon(Icons.map), 'Translucent full screen map');
+
+  @override
+  Widget build(BuildContext context) {
+    return const TranslucentFullMap();
+  }
+}
+
+class TranslucentFullMap extends StatefulWidget {
+  const TranslucentFullMap({super.key});
+
+  @override
+  State createState() => TranslucentFullMapState();
+}
+
+class TranslucentFullMapState extends State<TranslucentFullMap> {
+  final Completer<MapLibreMapController> mapController = Completer();
+  bool canInteractWithMap = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButtonLocation:
+          FloatingActionButtonLocation.miniCenterFloat,
+      floatingActionButton: canInteractWithMap
+          ? FloatingActionButton(
+              onPressed: _moveCameraToNullIsland,
+              mini: true,
+              child: const Icon(Icons.restore),
+            )
+          : null,
+      body: Stack(
+        children: [
+          const ColoredBox(
+            color: Colors.blue,
+            child: Center(
+              child: Text(
+                'Any widget can be here',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+          ),
+          MapLibreMap(
+            styleString: _styleString,
+            onMapCreated: (controller) => mapController.complete(controller),
+            initialCameraPosition: _nullIsland,
+            onStyleLoadedCallback: () =>
+                setState(() => canInteractWithMap = true),
+            // This is default value, but I set it here for example purposes.
+            foregroundLoadColor: Colors.purple,
+            translucentTextureSurface: true,
+          ),
+        ],
+      ),
+    );
+  }
+
+  String get _styleString => 'assets/translucent_style.json';
+
+  void _moveCameraToNullIsland() => mapController.future.then(
+      (c) => c.animateCamera(CameraUpdate.newCameraPosition(_nullIsland)));
+}
