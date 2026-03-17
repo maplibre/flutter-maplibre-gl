cc484619a0fce1fdd71d6befff769bcf93102c9d
Gabriel Palmisano <gabrielpalmisano63@gmail.com>
2026-03-17 12:26:46 +0100

    refactor: Update data type for annotations to Map<String, dynamic> and ensure toGeoJson includes properties data

--- maplibre_gl_platform_interface/lib/src/fill.dart
+++ maplibre_gl_platform_interface/lib/src/fill.dart
@@ -31,8 +31,8 @@ class Fill implements Annotation {
   @override
   String get id => _id;
 
-  final Map? _data;
-  Map? get data => _data;
+  final Map<String, dynamic>? _data;
+  Map<String, dynamic>? get data => _data;
 
   /// The fill configuration options most recently applied programmatically
   /// via the map controller.
@@ -46,6 +46,9 @@ class Fill implements Annotation {
     final geojson = options.toGeoJson();
     geojson["id"] = id;
     geojson["properties"]["id"] = id;
+    if (_data != null) {
+      geojson["properties"].addAll(_data);
+    }
 
     return geojson;
   }
