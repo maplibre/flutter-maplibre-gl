b52947490e4b0fc5fe932db8fc527f44236502f8
Gabriel Palmisano <gabrielpalmisano63@gmail.com>
2026-08-04 11:30:47 +0200

    style: wrap conditional offline export import to satisfy dart format

--- maplibre_gl/lib/maplibre_gl.dart
+++ maplibre_gl/lib/maplibre_gl.dart
@@ -48,7 +48,8 @@ import 'package:collection/collection.dart';
 import 'package:maplibre_gl_platform_interface/maplibre_gl_platform_interface.dart';
 
 // Offline database file copy needs dart:io on mobile; the web stub throws.
-import 'src/offline_export_stub.dart' if (dart.library.io) 'src/offline_export_io.dart';
+import 'src/offline_export_stub.dart'
+    if (dart.library.io) 'src/offline_export_io.dart';
 
 export 'package:maplibre_gl_platform_interface/maplibre_gl_platform_interface.dart'
     show
